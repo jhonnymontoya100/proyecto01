2b5ce9e997636588a5766823945df1a755256175
jhonnymontoya <jhonny_montoya23161@elpoli.edu.co>
2021-03-04 21:13:03 -0500

    primer commit creando proyecto01

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    <title>poyecto Heroku</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" content="text/css" href="css/normalize.css"/>
+    <link rel="stylesheet" content="text/css" href="css/estilos.css"/>
+    </head>
+
+<body>
+ <div id="envoltura">
+  <header>
+    <h1>programaci&oacute;n Web</h1>  
+  </header>
+  <div id="main">
+      <p>
+        En este curso aprende&aacute;s a programar p&aacute;ginas web usando git.
+         
+      <?php
+            $curso = "contruccion de elementos 2";
+      echo "Estas en el curso: $curso"  ?>
+ 
+</p>
+     
+      </div>
+      
+       </div>
+
+
+</body>
+</html>
