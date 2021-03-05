a609f9efa73ca40862e11e879e89b218a045ab9c
jhonnymontoya <jhonny_montoya23161@elpoli.edu.co>
2021-03-04 22:16:55 -0500

    Commig 2: agregando archivo pagina2.php con algo de concatenacion

---
+++ pagina2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    <title>poyecto Heroku</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" content="text/css" href="css/normalize.css"/>
+    <link rel="stylesheet" content="text/css" href="css/estilos.css"/>
+    </head>
+
+<body>
+ 
+</html>
