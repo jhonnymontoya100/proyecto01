59fdbba3cdecf0a99df849be130170eaa6893aac
jhonnymontoya <jhonny_montoya23161@elpoli.edu.co>
2021-03-09 21:01:47 -0500

    Comment number four, update of the styles fronend

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-    <title>poyecto Heroku</title>
-    <meta charset="utf-8"/>
+    <title>proyecto Heroku</title>
+    <meta charset="utf-8" />
     <link rel="stylesheet" content="text/css" href="css/normalize.css"/>
     <link rel="stylesheet" content="text/css" href="css/estilos.css"/>
     </head>
 
 <body>
- <div id="envoltura">
+<div id="envoltura">
   <header>
     <h1>programaci&oacute;n Web</h1>  
   </header>
+
   <div id="main">
       <p>
         En este curso aprende&aacute;s a programar p&aacute;ginas web usando git.
@@ -21,14 +22,14 @@
       
             $curso = "contruccion de elementos 2";
       echo "Estas en el curso: $curso"  
-     
+
       ?>
- 
-</p>
+
+      </p>
      
-      </div>
+  </div>
       
-       </div>
+</div>
 
 
 </body>
